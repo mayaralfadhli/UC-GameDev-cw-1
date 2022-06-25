db03042d961365b543c69298217b8c0006ffbc3d
mayaralfadhli <84667222+mayaralfadhli@users.noreply.github.com>
2022-06-25 13:08:08 +0300

    3D Car, VS code

---
+++ Untitled-1.cs
@@ -0,0 +1,21 @@
+
+string heroName = "mayar";
+Console.WriteLine("hero Name" + heroName);
+int heroHeight = 167;
+Console.WriteLine("hero Height" + heroHeight);
+int heroAge = 23;
+Console.WriteLine("hero Age" + heroAge);
+string heroSuperPower = "disappearness";
+Console.WriteLine("hero Super Power" + heroSuperPower);
+
+
+     string villainName = "vie";
+     Console.WriteLine("villain Name" + villainName);
+     int vililanHeight = 190;
+     Console.WriteLine("villain Height" + vililanHeight);
+     int villainAge = 45;
+     Console.WriteLine("villain Age" + villainAge);
+     string villainSuperPower = "fast";
+     Console.WriteLine("villainSuperPower" + villainSuperPower);
+     string ageDifference = villainAge - heroAge;
+     Console.WriteLine("age diiference" + ageDifference);
